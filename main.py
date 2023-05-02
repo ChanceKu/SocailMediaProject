038a043ef023c9d3e99918133700b37bdc5b015a
ChanceKu <kuhonman609@gmail.com>
2023-05-03 00:49:06 +0800

    fixed a bug

--- main.py
+++ main.py
@@ -42,10 +42,10 @@ for result in results[:i]:
 
     # 按排序->最新
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[3]/div[7]/div[2]/button/span/span"))).click()
-    driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div[1]/div[2]").click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[3]/div[1]/div[2]"))).click()
 
     time.sleep(1)
-    
+
     #打開全文
     try:
         button = driver.find_elements(By.TAG_NAME, 'button')
@@ -82,10 +82,6 @@ for result in results[:i]:
         # 服務
         try:
             service = comment.find_elements(By.CLASS_NAME, "RfDO5c")
-        except:
-            service = "None"
-
-        try:
             #各細節
             skip = False
             detail = []
@@ -107,7 +103,31 @@ for result in results[:i]:
                 else:
                     skip = False
         except:
-            print("Error:3")
+            service = "None"
+
+        # try:
+        #     #各細節
+        #     skip = False
+        #     detail = []
+        #     for i in range(0, len(service)):
+        #         if skip == False:
+        #             if "：" in service[i].text:
+        #                 split_string = service[i].text.split("：")
+        #                 ser = split_string[0]
+        #                 vice = split_string[1]
+        #                 detail.append(ser)
+        #                 detail.append(vice)
+        #             else:
+        #                 ser = service[i].text
+        #                 vice = service[i+1].text
+        #                 detail.append(ser)
+        #                 detail.append(vice)                
+        #                 skip = True
+        #             # print(ser, ":", vice)
+        #         else:
+        #             skip = False
+        # except:
+        #     print("Error:3")
         
         try:
             comments_data.append({
