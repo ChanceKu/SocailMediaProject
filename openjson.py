4d57203d4de986fcc734c668266e375644610fb7
ChanceKu <kuhonman609@gmail.com>
2023-05-03 23:21:07 +0800

    update

--- openjson.py
+++ openjson.py
@@ -7,6 +7,7 @@ with open('Gmaps.json', 'r', encoding='utf-8') as f:
 
 # Convert JSON to DataFrame
 df = pd.DataFrame(data)
+name = df["名字"]
 
-# Print the DataFrame
-print(df.head())
+import collections
+print([item for item, count in collections.Counter(name).items() if count > 1])
